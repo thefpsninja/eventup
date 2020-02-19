f11fc7b9c84bd8595fa23ced8b61409cca36a56a
thefpsninja <60346906+thefpsninja@users.noreply.github.com>
2020-02-19 13:02:21 +0100

    First Commit
    First Commit, Added our Mockup

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="styles/style.css" type="text/css">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container"> <button class="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar12">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbar12"> <a class="navbar-brand d-none d-md-block" href="#">
+          <i class="fa d-inline fa-lg fa-circle"></i>
+          <b> EventUp</b>
+        </a>
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item m-1 border rounded border-secondary"> <a class="nav-link bg-secondary text-white" href="#">Skapa Event</a> </li>
+        </ul>
+        <a class="btn btn-default navbar-btn btn-secondary text-white" data-toggle="modal">Login</a><a class="btn btn-default navbar-btn text-white btn-primary">Registrera<br></a>
+      </div>
+    </div>
+  </nav>
+  <div class="py-5 d-flex" style="	background-image: url(&quot;https://images.unsplash.com/photo-1579239314215-7436539448b6?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjExMDk0fQ&amp;auto=format&amp;fit=crop&amp;w=634&amp;q=80&quot;);	background-position: center;	background-size: 100%;	background-repeat: repeat;">
+    <div class="container w-25" style="	max-width: 15%;	min-width: 10%;">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="btn-group" style="width: 100%;">
+            <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown"> Kategorier<br></button>
+            <div class="dropdown-menu"> <a class="dropdown-item" href="#">ALLA<br></a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">PARTY</a>
+              <a class="dropdown-item" href="#">MUSIC</a><a class="dropdown-item" href="#">FIKA</a><a class="dropdown-item" href="#">ANIME</a><a class="dropdown-item" href="#">GAMING<br></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container d-flex">
+      <div class="row flex-grow-0" style="">
+        <div class="col-md-11" style="">
+          <ul class="list-group">
+            <li class="list-group-item d-flex justify-content-between align-items-center">MALMÖ: Dropin Party! BBQ&nbsp;<span class="badge badge-pill badge-primary"></span><span class="badge badge-pill badge-primary">25 Deltagare</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">LUND: Cosplay Convention&nbsp;<span class="badge badge-pill badge-primary"></span><span class="badge badge-pill badge-primary">634 Deltagare</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center" style="">HELSINGBORG: Tabeltop Tournament! WARHAMMER&nbsp;<span class="badge badge-pill badge-primary">159 Deltagare</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center" style="">HELSINGBORG: Tabeltop Tournament! WARHAMMER&nbsp;<span class="badge badge-pill badge-primary">159 Deltagare</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center" style="">HELSINGBORG: Tabeltop Tournament! WARHAMMER&nbsp;<span class="badge badge-pill badge-primary">159 Deltagare</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center" style="">HELSINGBORG: Tabeltop Tournament! WARHAMMER&nbsp;<span class="badge badge-pill badge-primary">159 Deltagare</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center" style="">HELSINGBORG: Tabeltop Tournament! WARHAMMER&nbsp;<span class="badge badge-pill badge-primary">159 Deltagare</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center" style="">HELSINGBORG: Tabeltop Tournament! WARHAMMER&nbsp;<span class="badge badge-pill badge-primary">159 Deltagare</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center" style="">HELSINGBORG: Tabeltop Tournament! WARHAMMER&nbsp;<span class="badge badge-pill badge-primary">159 Deltagare</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center" style="">HELSINGBORG: Tabeltop Tournament! WARHAMMER&nbsp;<span class="badge badge-pill badge-primary">159 Deltagare</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center" style="">HELSINGBORG: Tabeltop Tournament! WARHAMMER&nbsp;<span class="badge badge-pill badge-primary">159 Deltagare</span></li>
+          </ul>
+        </div>
+      </div>
+      <div class="row" style="">
+        <div class="col-md-11" style="">
+          <ul class="list-group">
+            <li class="list-group-item d-flex justify-content-between align-items-center">MALMÖ: Dropin Party! BBQ&nbsp;<span class="badge badge-pill badge-primary"></span><span class="badge badge-pill badge-primary">25 Deltagare</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">LUND: Cosplay Convention&nbsp;<span class="badge badge-pill badge-primary"></span><span class="badge badge-pill badge-primary">634 Deltagare</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center" style="">HELSINGBORG: Tabeltop Tournament! WARHAMMER&nbsp;<span class="badge badge-pill badge-primary">159 Deltagare</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center" style="">HELSINGBORG: Tabeltop Tournament! WARHAMMER&nbsp;<span class="badge badge-pill badge-primary">159 Deltagare</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center" style="">HELSINGBORG: Tabeltop Tournament! WARHAMMER&nbsp;<span class="badge badge-pill badge-primary">159 Deltagare</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center" style="">HELSINGBORG: Tabeltop Tournament! WARHAMMER&nbsp;<span class="badge badge-pill badge-primary">159 Deltagare</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center" style="">HELSINGBORG: Tabeltop Tournament! WARHAMMER&nbsp;<span class="badge badge-pill badge-primary">159 Deltagare</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center" style="">HELSINGBORG: Tabeltop Tournament! WARHAMMER&nbsp;<span class="badge badge-pill badge-primary">159 Deltagare</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center" style="">HELSINGBORG: Tabeltop Tournament! WARHAMMER&nbsp;<span class="badge badge-pill badge-primary">159 Deltagare</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center" style="">HELSINGBORG: Tabeltop Tournament! WARHAMMER&nbsp;<span class="badge badge-pill badge-primary">159 Deltagare</span></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <pingendo onclick="window.open('https://pingendo.com/', '_blank')" style="cursor:pointer;position: fixed;bottom: 20px;right:20px;padding:4px;background-color: #00b0eb;border-radius: 8px; width:220px;display:flex;flex-direction:row;align-items:center;justify-content:center;font-size:14px;color:white">Made with Pingendo Free&nbsp;&nbsp;<img src="https://pingendo.com/site-assets/Pingendo_logo_big.png" class="d-block" alt="Pingendo logo" height="16"></pingendo>
+</body>
+
+</html>
