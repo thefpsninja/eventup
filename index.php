3785d7c4ba496c5e4022ab99d64e7e516f024168
thefpsninja <60346906+thefpsninja@users.noreply.github.com>
2020-02-24 12:53:17 +0100

    Giving Homebutton functionallity
    By refering it to Index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <div class="container"> <button class="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar12">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbar12"> <a class="navbar-brand d-none d-md-block" href="#">
+      <div class="collapse navbar-collapse" id="navbar12"> <a class="navbar-brand d-none d-md-block" href="index.php">
           <i class="fa d-inline fa-lg fa-circle"></i>
           <b> EventUp</b>
         </a>
