189a67fd43f6df58ac3d283436247a739ec59cb1
natanori <laura2norden@gmail.com>
2020-02-24 14:07:07 +0100

    Ändrat språk till svenska

--- register.php
+++ register.php
@@ -17,28 +17,28 @@
           <div class="image-holder"></div>
 
             <form method="post">
-              <h2 class="text-center"><strong>Create</strong> an account.</h2>
+              <h2 class="text-center"><strong>Skapa</strong> ett event-konto</h2>
               
               <div class="form-group">
                 <input class="form-control" type="email" name="email" placeholder="Email" />
               </div>
               <div class="form-group">
-                <input class="form-control" type="password" name="password" placeholder="Password" />
+                <input class="form-control" type="password" name="password" placeholder="Lösenord" />
               </div>
               <div class="form-group">
-                <input class="form-control" type="password" name="password-repeat" placeholder="Password (repeat)" />
+                <input class="form-control" type="password" name="password-repeat" placeholder="Lösenord (uprepa)" />
               </div>
               <div class="form-group">
                 <div class="form-check">
                   <label class="form-check-label">
-                    <input class="form-check-input" type="checkbox" />I agree to the license terms.</label>
+                    <input class="form-check-input" type="checkbox" />Jag godkänner licensvillkoren.</label>
                 </div>
               </div>
 
               <div class="form-group">
-                <button class="btn btn-primary btn-block" type="submit">Sign Up</button>
+                <button class="btn btn-primary btn-block" type="submit">Bli medlem</button>
               </div>
-              <a class="already" href="#">You already have an account? Login here.</a>
+              <a class="already" href="#">Du har redan ett konto? Logga in här.</a>
 
             </form>
           </div>
@@ -47,7 +47,7 @@
 
       <!-- Modal footer -->
       <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Stäng</button>
       </div>
 
     </div>
