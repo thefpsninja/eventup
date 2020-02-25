7f7d5cc1a00597e324a4428ca28bb235d82b67a0
thefpsninja <60346906+thefpsninja@users.noreply.github.com>
2020-02-25 10:47:18 +0100

    Events now get posted from database

--- printEvent.php
+++ printEvent.php
@@ -1,71 +1,36 @@
-<div class="w-75 view overlay">
-      <li type="button" class="list-group-item d-flex justify-content-between align-items-center" data-toggle="collapse" href="#collapseOne">
-     MALMÖ: Dropin Party! BBQ&nbsp;<span class="badge badge-pill badge-primary"></span><span class="badge badge-pill badge-primary">25 Deltagare</span> </li>
-    <div id="collapseOne" class="collapse" data-parent="#accordion">
-      <div class="list-group-item">
-        <img src="https://i.kym-cdn.com/entries/icons/original/000/000/007/bd6.jpg" class="img-thumbnail w-50 mx-auto d-block" alt="RickRoll">
-        <?
+<div class="">
+<?
 require('database.php');
-$sql = "SELECT * FROM eventUp";
+$sql = "SELECT * FROM events";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
-$events = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$eventPost = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <pre>
     <?php
-    print_r($events);
+    // print_r($eventPost);
     ?>
 </pre>
 <?
-foreach($events as $event) : 
+foreach($eventPost as $event) : 
     if($event['eventName']) : ?>
-        <h1>
-            <? echo $event['eventName']; ?>
-        </h1>
-        <h2>
-            <? echo $event['content']; ?>
-        </h2>
-        <p>
-            <? echo $event['dates']; ?>
-        </p>
-        <h4>
-            <? echo $event['id']; ?>
-        </h4>
+    <div class="w-75 view overlay">
+      <li type="button" class="list-group-item d-flex justify-content-between align-items-center" data-toggle="collapse" href="#collapse<? echo $event['id']; ?>">
+      <? echo $event['eventName']; ?><span class="badge badge-pill badge-primary"></span><span class="badge badge-pill badge-primary">25 Deltagare</span> </li>
+    <div id="collapse<? echo $event['id']; ?>" class="collapse" data-parent="#accordion">
+      <div class="list-group-item">
+        <img src="<? 
+        if (empty($event['images'])) 
+        {echo 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRV0IIzEdZ1yi-_SOEp6qLDx35PQhehqMC_rN8ftlHpMGf3ioup';} 
+        else {echo $event['images'];} ?>" class="img-thumbnail w-50 mx-auto d-block" alt="">
+        <p class="lead"> <? echo $event['content']; ?> </p>
+      </div>
+    </div>
+  </div>
         <!-- <form action="deleteEvent.php" method="post">
             <input type="hidden" name="send" value="<? //echo $event['id']; ?>">
             <button>Radera eventet</button>
         </form> -->
     <? endif; ?>
 <? endforeach; ?>
-
-      
-
-<!--         <form action="getsushi.php">
-
-            <button id="btn-dish-id" name="btn-delete" value="delete" type="submit" class="btn btn-primary">Delete</button> -->
-
-            <?
-            /* if($_POST['btn-delete']=='id'){
-                mysql_query("delete from sushi_menu where id='{$_POST["dish-id"]}'");
-            }
-                $sql = "DELETE FROM sushi_menu WHERE id=('dish-id')";  */
-            ?>
-
-    <!--  </form> -->
-<?
-
-//foreach($curlybrackets as $curlybracket) {
-    // do something }
-
-// foreach($curlybrackets as $curlybracket) :
-//     if ($var == "?") : 
-//         // do something
-
-//         else :
-//             // do something else
-//     endif;   
-// endforeach;
-?>
-      </div>
-    </div>
-  </div>
+</div>
