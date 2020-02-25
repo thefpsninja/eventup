eed0598ee216f9572a19185203e5de1541d13539
thefpsninja <60346906+thefpsninja@users.noreply.github.com>
2020-02-25 13:35:50 +0100

    Event Footer with SignUp

--- printEvent.php
+++ printEvent.php
@@ -4,15 +4,15 @@ require('database.php');
 $sql = "SELECT * FROM events";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
-$eventPost = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$eventPosts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <pre>
     <?php
-    // print_r($eventPost);
+    // print_r($eventPosts);
     ?>
 </pre>
 <?
-foreach($eventPost as $event) : 
+foreach($eventPosts as $event) : 
     if($event['eventName']) : ?>
     <div class="w-75 view overlay">
       <li type="button" class="list-group-item d-flex justify-content-between align-items-center" data-toggle="collapse" href="#collapse<? echo $event['id']; ?>">
@@ -24,6 +24,7 @@ foreach($eventPost as $event) :
         {echo 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRV0IIzEdZ1yi-_SOEp6qLDx35PQhehqMC_rN8ftlHpMGf3ioup';} 
         else {echo $event['images'];} ?>" class="img-thumbnail w-50 mx-auto d-block" alt="">
         <p class="lead"> <? echo $event['content']; ?> </p>
+        <div class="card-footer"><? include 'participantForm.php'; ?></div>
       </div>
     </div>
   </div>
