8e3e926ecbff674dadd2271e91a8bd8da154a6e2
natanori <laura2norden@gmail.com>
2020-02-27 02:51:23 +0100

    Update participantForm.php
    Laggt till form-validation

--- participantForm.php
+++ participantForm.php
@@ -9,7 +9,12 @@ require ('database.php');
         <h2 class="text-center">Jag vill vara med!</h2>
 
         <div class="form-group">
-            <input type="text" class="form-control" name="participantName" placeholder="Namn" require/>
+            <input type="text" data-toggle="tooltip" class="form-control" name="participantName" placeholder="Ditt namn" autofocus required title="Ange ditt namn" />
+        </div>
+
+
+        <!-- <div class="form-group"> -->
+            <!-- <input type="text" class="form-control" name="participantName" placeholder="Namn" require/> -->
             <? 
             // name validation
             /* $name = test_input($_POST["name"]);
@@ -17,12 +22,17 @@ require ('database.php');
             $nameErr = "Endast bokstäver och vitt utrymme tillåtet"; 
             } */
             ?>
-        </div>
+        <!-- </div> -->
 
         
-
         <div class="form-group">
-            <input type="email" class="form-control" name="participantEmail" placeholder="Email" require/>
+            <input type="email" data-toggle="tooltip" class="form-control" placeholder="t.ex.: ditt.namn@domain.com" name="participantEmail" autofocus required title="Ange ditt email-adress" />
+            <input type="hidden" name="eventID" value="<? echo $event['id']; ?>">
+        </div>
+
+
+        <!-- <div class="form-group"> -->
+            <!-- <input type="email" class="form-control" name="participantEmail" placeholder="Email" require/> -->
             <? 
             // email validation
             /* $email = test_input($_POST["email"]);
@@ -30,8 +40,8 @@ require ('database.php');
             $emailErr = "Ange en korrekt e-postadress."; 
             } */
             ?>
-            <input type="hidden" name="eventID" value="<? echo $event['id']; ?>">
-        </div>
+            <!-- <input type="hidden" name="eventID" value="<? //echo $event['id']; ?>">
+        </div> -->
     <div class="form-group">
         <button  class="btn btn-primary" type="submit">Skicka</button>
     </div>
